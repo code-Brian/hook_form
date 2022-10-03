df84de9e04b79f0fb695cb0d0a596d848a85ef53
Brian Denmark <testingarea479@gmail.com>
2022-10-02 21:58:55 -0700

    init commit

---
+++ src/components/PersonCard.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+import UserForm from './components/UserForm'
+
+const PersonCard = (props)=> {
+    const { firstName, lastName, email, password, confirmPassword } = props
+    
+    return(
+        <div>
+            <h2>Your Form Data</h2>
+            <ul>
+                <li>First Name: { firstName }</li>
+                <li>Last Name: { lastName }</li>
+                <li>Email: { email }</li>
+                <li>Password: { password }</li>
+                <li>Confirm Password: { confirmPassword }</li>
+            </ul>
+        </div>
+    )
+}
